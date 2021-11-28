e4594d0561ddbf83a82ba6f336c9011c6e5d3f03
Krzysztof Palka <palkakrzysiek@gmail.com>
2021-11-28 23:15:30 +0100

    hmac homework

--- src/main/java/_1_aes_sha/homework/Token.java
+++ src/main/java/_1_aes_sha/homework/Token.java
@@ -1,5 +1,11 @@
 package _1_aes_sha.homework;
 
+import _1_aes_sha.examples.sha.Utils;
+import org.bouncycastle.jcajce.provider.digest.SHA3;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 /**
  * Copyright Adam Smolarek chaindev.pl
  */
@@ -14,14 +20,16 @@ public class Token {
         this.secret = secret;
     }
 
+    SHA3.DigestSHA3 sha3 = new SHA3.DigestSHA3(256);
+
     public String issue(String msg) {
-        //praca domowa
-        return null;
+        byte[] saltedMsg = Utils.concat(secret.getBytes(StandardCharsets.UTF_8),msg.getBytes(StandardCharsets.UTF_8));
+        byte[] hashed = sha3.digest(saltedMsg);
+        return Base64.getEncoder().encodeToString(hashed);
     }
 
     public Boolean isValid(String mac, String msg) {
-        //praca domowa
-        return null;
+        return mac.equals(issue(msg));
     }
 
 }
--- src/test/java/_1_aes_sha/homework/TokenTest.java
+++ src/test/java/_1_aes_sha/homework/TokenTest.java
@@ -1,4 +1,48 @@
-import static org.junit.jupiter.api.Assertions.*;
+package _1_aes_sha.homework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class TokenTest {
-  
+
+    String msg = "msg 1";
+    String msg2 = "msg 2";
+    String secret = "secret 1";
+    String secret2 = "secret 2";
+
+
+    @Test
+    public void hmacShouldBeInBase64Format() {
+        assertThat(new Token(secret).issue(msg))
+            .isEqualTo("y1OiWlQmQICfRlptOskqRhOG90rKFfEZC50mKgOhCk4=");
+    }
+
+    @Test
+    public void hmacIsDifferentForDifferentSecrets() {
+        assertThat(new Token(secret).issue(msg))
+            .isNotEqualTo(new Token(secret2).issue(msg));
+    }
+
+    @Test
+    public void hmacIsDifferentForDifferentMessages() {
+        assertThat(new Token(secret).issue(msg))
+            .isNotEqualTo(new Token(secret).issue(msg2));
+    }
+
+    @Test
+    public void hmacIsPositivelyValidatedForTheSameMessage() {
+        var token = new Token(secret);
+        String hmac = token.issue(msg);
+        assertThat(token.isValid(hmac, msg)).isTrue();
+    }
+
+    @Test
+    public void hmacIsNegativelyValidatedForDifferentMessage() {
+        var token = new Token(secret);
+        String hmac = token.issue(msg);
+        assertThat(token.isValid(hmac, msg2)).isFalse();
+    }
+
+
 }
